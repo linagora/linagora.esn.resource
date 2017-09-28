6815821109ba370432fab4746bc829f128849b87
Christophe Hamerling <chamerling@linagora.com>
2017-09-28 15:40:29 +0200

    linagora/lgs/openpaas/linagora.esn.calendar#885 Fix the sort key

--- backend/lib/constants.js
+++ backend/lib/constants.js
@@ -8,7 +8,7 @@ module.exports = {
     INDEX_NAME: 'resources.idx',
     TYPE_NAME: 'resources',
     DEFAULT_LIMIT: 20,
-    DEFAULT_SORT_KEY: 'start',
+    DEFAULT_SORT_KEY: 'name',
     DEFAULT_SORT_ORDER: 'desc'
   }
 };

3036554112c77af4b681ac3f3aa2e0d1661349a7
Christophe Hamerling <chamerling@linagora.com>
2017-10-04 14:42:54 +0200

    linagora/lgs/openpaas/linagora.esn.calendar#907 Add the esn-resource-list component

---
+++ frontend/app/list/item/resource-list-item.component.js
@@ -0,0 +1,11 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.resource').component('esnResourceListItem', {
+    bindings: {
+      resource: '<'
+    },
+    controllerAs: 'ctrl',
+    templateUrl: '/linagora.esn.resource/app/list/item/resource-list-item.html'
+  });
+})(angular);

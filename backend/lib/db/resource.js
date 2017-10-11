36d5e94bcda135f6c66ca6fa99277ddbd5442d7d
Christophe Hamerling <chamerling@linagora.com>
2017-10-11 10:41:04 +0200

    linagora/lgs/openpaas/linagora.esn.calendar#913 Add administrator array in model

--- backend/lib/db/resource.js
+++ backend/lib/db/resource.js
@@ -9,6 +9,12 @@ module.exports = dependencies => {
     type: {type: String, required: true},
     domain: {type: ObjectId, ref: 'Domain', required: true},
     creator: {type: ObjectId, ref: 'User', required: true},
+    administrators: [
+      {
+        objectType: {type: String, required: true},
+        id: {type: mongoose.Schema.Types.Mixed, required: true}
+      }
+    ],
     timestamps: {
       creation: {type: Date, default: Date.now},
       updatedAt: {type: Date}

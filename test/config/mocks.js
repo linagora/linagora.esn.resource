0b48bd57731bfdb1a867d0d59441c607f0f423a2
Maxime Frere <mfrere@linagora.com>
2017-11-13 11:42:20 +0100

    linagora/lgs/openpaas/linagora.esn.calendar#1014 update frontend test conf

--- test/config/mocks.js
+++ test/config/mocks.js
@@ -1,13 +1,3 @@
 'use strict';
 
 angular.module('esn.form.helper', []);
-angular.module('esn.attendee', []).factory('attendeeService', function() {});
-angular.module('pascalprecht.translate', [])
-  .provider('$translate', function() {
-    return {
-      useSanitizeValueStrategy: angular.noop,
-      preferredLanguage: angular.noop,
-      useStaticFilesLoader: angular.noop,
-      $get: angular.noop
-    };
-  });

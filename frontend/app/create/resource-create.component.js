53db64b53ebbf65c70ad6aaf560bdc5e60146543
Maxime Frere <mfrere@linagora.com>
2017-11-14 13:55:06 +0100

    linagora/lgs/openpaas/linagora.esn.calendar#967 Fix scope create controller

--- frontend/app/create/resource-create.component.js
+++ frontend/app/create/resource-create.component.js
@@ -6,9 +6,28 @@
       bindings: {
         type: '=?'
       },
-      controller: 'ESNResourceCreateController',
+      controller: ComponentController,
       controllerAs: 'ctrl',
       transclude: true,
       templateUrl: '/linagora.esn.resource/app/create/resource-create.html'
     });
+
+    function ComponentController($modal) {
+      var self = this;
+
+      self.openResourceCreateModal = openResourceCreateModal;
+
+      function openResourceCreateModal() {
+        $modal({
+          templateUrl: '/linagora.esn.resource/app/components/resource-form-modal/resource-form-modal.html',
+          controller: 'ESNResourceFormCreateController',
+          backdrop: 'static',
+          placement: 'center',
+          controllerAs: 'ctrl',
+          resolve: {
+            type: function() { return self.type; }
+          }
+        });
+      }
+    }
 })(angular);
--- frontend/app/create/resource-create.controller.js
+++ frontend/app/create/resource-create.controller.js
@@ -1,63 +1,25 @@
-(function() {
+(function(angular) {
   'use strict';
 
   angular.module('linagora.esn.resource')
-    .controller('ESNResourceCreateController', ESNResourceCreateController);
+    .controller('ESNResourceCreateModalController', ESNResourceCreateModalController);
 
-  function ESNResourceCreateController($modal, $scope, $state, _, esnResourceAPIClient, asyncAction, session, ESN_RESOURCE) {
+  function ESNResourceCreateModalController($modal) {
     var self = this;
 
-    self.resource = {};
-    self.beAdmin = true;
-    self.submit = submit;
-    self.resourceAdministrators = [];
-    self.resourceTypes = ESN_RESOURCE.TYPES;
-    self.defaultResourceType = 'resource';
-    self.selectedType = self.type || 'resource';
-    self.isSelected = isSelected;
     self.openResourceCreateModal = openResourceCreateModal;
 
-    function isSelected(type) {
-      return type === self.selectedType;
-    }
-
-    function submit() {
-      return asyncAction({
-        progressing: 'Creating resource...',
-        success: 'Resource has been created',
-        failure: 'Failed to create resource'
-      }, function() {
-        self.resource.type = self.selectedType;
-        self.resource.administrators = _.map(self.resourceAdministrators, function(admin) {
-          return {
-            id: admin._id,
-            objectType: 'user'
-          };
-        });
-
-        if (self.beAdmin) {
-          self.resource.administrators.push({
-            id: session.user._id,
-            objectType: 'user'
-          });
-        }
-
-        return esnResourceAPIClient.create(self.resource).finally(function() {
-          $state.reload();
-        });
-      });
-    }
-
     function openResourceCreateModal() {
-      self.modal = $modal({
+      $modal({
         templateUrl: '/linagora.esn.resource/app/components/resource-form-modal/resource-form-modal.html',
-        controller: function() {
-          angular.extend(this, self);
-        },
+        controller: 'ESNResourceFormCreateController',
         backdrop: 'static',
         placement: 'center',
-        controllerAs: 'ctrl'
+        controllerAs: 'ctrl',
+        locals: {
+          type: self.type
+        }
       });
     }
   }
-})();
+})(angular);

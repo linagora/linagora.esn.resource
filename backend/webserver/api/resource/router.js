f3e42cb4fd46c5f3de4d8578289603b61db6e472
Aymane Khouaji <akhouaji@linagora.com>
2018-04-04 11:35:10 +0200

    #6 Middleware to check if the current user is a domain admin

--- backend/webserver/api/resource/router.js
+++ backend/webserver/api/resource/router.js
@@ -6,10 +6,13 @@ module.exports = dependencies => {
   const controller = require('./controller')(dependencies);
   const middleware = require('./middleware')(dependencies);
   const authorizationMW = dependencies('authorizationMW');
+  const domainMW = dependencies('domainMW');
   const router = express.Router();
 
   router.post('/',
     authorizationMW.requiresAPILogin,
+    domainMW.loadSessionDomain,
+    authorizationMW.requiresDomainManager,
     middleware.canCreateResource,
     middleware.validateAdministrators,
     controller.create);
@@ -26,14 +29,16 @@ module.exports = dependencies => {
 
   router.delete('/:id',
     authorizationMW.requiresAPILogin,
+    domainMW.loadSessionDomain,
+    authorizationMW.requiresDomainManager,
     middleware.load,
-    middleware.canDeleteResource,
     controller.remove);
 
   router.put('/:id',
     authorizationMW.requiresAPILogin,
+    domainMW.loadSessionDomain,
+    authorizationMW.requiresDomainManager,
     middleware.load,
-    middleware.canUpdateResource,
     controller.update);
 
   return router;
